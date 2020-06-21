8488782c3264f6e4548fa08b9c644328531c1230
trypes <19194653+trypes@users.noreply.github.com>
2020-06-21 20:47:40 +0200

    Handled parsing errors appropriately

--- src/parse.rs
+++ src/parse.rs
@@ -15,57 +15,74 @@ use std::iter::Iterator;
 
 pub fn parse_bazaraki(html: &Html, url: &Url) -> Result<Listing, Error> {
   // Common regular expressions
-  let re_int = Regex::new(r"[0-9]+").unwrap();
+  let re_int = Regex::new(r"[0-9]+").expect("INTERNAL ERROR: Couldn't build regex");
 
   // Common selectors
-  let li_sel = Selector::parse("li").unwrap();
-  let a_sel = Selector::parse("a").unwrap();
-  let span_sel = Selector::parse("span").unwrap();
+  let li_sel = Selector::parse("li").expect("INTERNAL ERROR: Couldn't parse selector");
+  let a_sel = Selector::parse("a").expect("INTERNAL ERROR: Couldn't parse selector");
+  let span_sel = Selector::parse("span").expect("INTERNAL ERROR: Couldn't parse selector");
 
   // Parse UID
-  let id_sel: Selector = Selector::parse("span[itemprop=\"sku\"").unwrap();
+  let id_sel: Selector =
+    Selector::parse("span[itemprop=\"sku\"").expect("INTERNAL ERROR: Couldn't parse selector");
   let id: String = format!(
     "bazaraki_{}",
-    html.select(&id_sel).next().unwrap().inner_html()
+    html
+      .select(&id_sel)
+      .next()
+      .ok_or(Error::from("Couldn't select UID element"))?
+      .inner_html()
   );
 
   // Get timestamp
   let timestamp = Utc::now();
 
   // Parse price
-  let price_sel: Selector = Selector::parse("meta[itemprop=\"price\"]").unwrap();
+  let price_sel: Selector =
+    Selector::parse("meta[itemprop=\"price\"]").expect("INTERNAL ERROR: Couldn't parse selector");
   let price_str: &str = html
     .select(&price_sel)
     .next()
-    .unwrap()
+    .ok_or(Error::from("Couldn't select price element"))?
     .value()
     .attr("content")
-    .unwrap();
-  let price: u32 = price_str.parse::<f32>().unwrap() as u32;
+    .ok_or(Error::from("Couldn't get price's 'content' attribute"))?;
+  let price: u32 = price_str.parse::<f32>().map_err(|e| Error::from(e))? as u32;
 
   // Parse area
-  let area_sel: Selector = Selector::parse("span[itemprop=\"address\"]").unwrap();
-  let area_str = html.select(&area_sel).next().unwrap().inner_html();
+  let area_sel: Selector =
+    Selector::parse("span[itemprop=\"address\"]").expect("INTERNAL ERROR: Couldn't parse selector");
+  let area_str = html
+    .select(&area_sel)
+    .next()
+    .ok_or(Error::from("Couldn't select area element"))?
+    .inner_html();
   let area = match Area::lookup(&area_str) {
     Some(area) => area,
-    None => panic!("Couldn't parse area for {}", url),
+    None => return Err(Error::from("Couldn't parse area for")),
   };
 
   // Get useful html handles
-  let chars_sel = Selector::parse("div.announcement-characteristics").unwrap();
-  let chars = html.select(&chars_sel).next().unwrap();
+  let chars_sel = Selector::parse("div.announcement-characteristics")
+    .expect("INTERNAL ERROR: Couldn't parse selector");
+  let chars = html
+    .select(&chars_sel)
+    .next()
+    .ok_or(Error::from("Couldn't select characteristics"))?;
   let chars_html = chars.inner_html();
-  let desc_sel = Selector::parse("div.announcement-description").unwrap();
-  let desc = html.select(&desc_sel).next().unwrap();
+  let desc_sel = Selector::parse("div.announcement-description")
+    .expect("INTERNAL ERROR: Couldn't parse selector");
+  let desc = html
+    .select(&desc_sel)
+    .next()
+    .ok_or(Error::from("Couldn't select description element"))?;
   let desc_html = desc.inner_html();
 
   // Parse property kind
-  let kind = Kind::lookup(&chars_html)
-    .ok_or("Couldn't parse kind")
-    .unwrap();
+  let kind = Kind::lookup(&chars_html).ok_or(Error::from("Couldn't parse kind"))?;
 
   // Parse size
-  let re_size = Regex::new(r"([0-9]+) m²").unwrap();
+  let re_size = Regex::new(r"([0-9]+) m²").expect("INTERNAL ERROR: Couldn't parse regex");
   let size = re_size
     .captures(&chars_html)
     .map(|g| g[1].parse::<f32>().map(|a| a as u32).ok())
@@ -76,30 +93,33 @@ pub fn parse_bazaraki(html: &Html, url: &Url) -> Result<Listing, Error> {
 
   // Parse bedrooms
   let n_bedrooms = if Regex::new(r"[Ss]tudio")
-    .unwrap()
+    .expect("INTERNAL ERROR: Couldn't parse regex")
     .find(&chars_html)
     .is_some()
   {
     Some(0)
   } else {
-    let re_bedrooms = Regex::new(r"[Bb]edrooms*").unwrap();
-    chars
+    let re_bedrooms = Regex::new(r"[Bb]edrooms*").expect("INTERNAL ERROR: Couldn't parse regex");
+    match chars
       .select(&li_sel)
       .filter(|li| re_bedrooms.find(&li.inner_html()).is_some())
       .next()
-      .map(|li| {
+    {
+      Some(li) => Some(
         li.select(&a_sel)
           .next()
-          .unwrap()
+          .ok_or(Error::from("Couldn't select bedrooms"))?
           .inner_html()
           .trim()
           .parse()
-          .unwrap()
-      })
+          .map_err(|e| Error::from(format!("Couldn't parse bedrooms:{}", e)))?,
+      ),
+      None => None,
+    }
   };
 
   // Parse bathrooms
-  let re_bathrooms = Regex::new(r"[Bb]athrooms*").unwrap();
+  let re_bathrooms = Regex::new(r"[Bb]athrooms*").expect("INTERNAL ERROR: Couldn't parse regex");
   let n_bathrooms = chars
     .select(&li_sel)
     .filter(|li| re_bathrooms.find(&li.inner_html()).is_some())
@@ -110,13 +130,14 @@ pub fn parse_bazaraki(html: &Html, url: &Url) -> Result<Listing, Error> {
         .next()
         .map(|span| {
           let str_bathrooms = span.inner_html();
-          str_bathrooms.trim().parse().unwrap()
+          str_bathrooms.trim().parse().ok()
         })
     })
+    .flatten()
     .flatten();
 
   // Parse post code
-  let re_post = Regex::new(r"[Pp]ostal\s+[Cc]ode").unwrap();
+  let re_post = Regex::new(r"[Pp]ostal\s+[Cc]ode").expect("INTERNAL ERROR: Couldn't parse regex");
   let post_code = chars
     .select(&li_sel)
     .filter(|li| re_post.find(&li.inner_html()).is_some())
@@ -127,13 +148,14 @@ pub fn parse_bazaraki(html: &Html, url: &Url) -> Result<Listing, Error> {
         .next()
         .map(|span| {
           let str_post_code = span.inner_html();
-          str_post_code.trim().parse().unwrap()
+          str_post_code.trim().parse().ok()
         })
     })
+    .flatten()
     .flatten();
 
   // Parse year
-  let re_year = Regex::new(r"(20[0-3][0-9])|(19[0-9][0-9])").unwrap();
+  let re_year = Regex::new(r"(20[0-3][0-9])|(19[0-9][0-9])").expect("Couldn't parse regex");
   let year = re_year
     .captures_iter(&desc_html)
     .filter_map(|c| c[0].parse::<u32>().ok())
